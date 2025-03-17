ce3bd5bc933876fec891d8b0682451b6850328e6
Elroy Cabalda <elroycabalda@gmail.com>
2025-03-17 10:41:35 +0800

    Updated deployment files

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ElroyCabalda.github.io',
+  site: 'https://elroycabalda.github.io',
   base: '/portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
